d9c4df1edf77bb033a1cee75b27c619950e1c252
VinzzOfficial <pribvinzz123@gmail.com>
2026-01-08 00:52:27 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,15 @@
-
 const express = require("express");
-const fs = require("fs");
 const path = require("path");
-
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 8080;
+
+// WAJIB: static public
 app.use(express.static(path.join(__dirname, "public")));
 
-const DB_FILE = "./database/db.json";
-if (!fs.existsSync(DB_FILE)) {
-  fs.writeFileSync(DB_FILE, JSON.stringify({ users: [] }, null, 2));
-}
-
-app.get("/api/test", (req, res) => {
-  res.json({ status: "OK", message: "Server running" });
+// WAJIB: route utama
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
